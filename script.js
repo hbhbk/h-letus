73ab1743378cc75474e08ad4547ea091cafde572
hbhbk <60351314+hbhbk@users.noreply.github.com>
2024-05-06 21:22:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function signIn(){
 
     let params = {
         "client_id":"899421234400-9sp8p4b0ltnpm57bjlljcavmp9ng34lt.apps.googleusercontent.com",
-        "redirect_uri":"http://haaletus.euprofile.html",
+        "redirect_uri":"http://haaletus.eu/profile.html",
         "response_type":"token",
         "scope":"https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
         "include_granted_scopes": 'true',
